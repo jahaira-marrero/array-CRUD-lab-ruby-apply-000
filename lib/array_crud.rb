399da60ae570a6d18bb98e6417eb6fe6c15d2524
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-11 03:28:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end 
 
 def add_element_to_end_of_array(array)
-   array << "element"
+   array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
 end
 puts (["1", "2", "3", "4"])
 
