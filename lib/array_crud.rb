4594e9c67f01d4b90ecbc276807f415c3cac8dff
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-11 03:37:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,9 +25,9 @@ end
 def retrieve_element_from_index(array, index_number)
   array[index_number]
 end
- array[0]
+ 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
